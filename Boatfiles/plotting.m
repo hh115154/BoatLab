679faf53e904330dda7800b5855f235dd35f2b25
Sondre <sondrehf@ntnu.stud.no>
2018-10-23 14:18:59 +0200

    small changes

--- Boatfiles/plotting.m
+++ Boatfiles/plotting.m
@@ -1,6 +1,11 @@
 plot(compass.time, compass.signals.values);
-axis([0 1000 -550 50])
+axis([0 5000 -4 70])
 grid on
 ylabel('Compass Angle [Deg]')
 xlabel('Time [s]')
-title('Compass angle response with sine input with frequency 0.05rad/s and noise')
+title('Compass angle response with sine (freq. 0.005rad/s) and wave/measurement noise')
+
+fs = 10*
+x = psi_w
+
+[pxx,f] = pwelch(x,window, [], [],fs)
