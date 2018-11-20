e3439795e7a4e818a2a3c43fcafd8978caeb610d
Sjurinho Wroldsen <sjurgw@stud.ntnu.no>
2018-11-20 13:15:03 +0100

    plots and simulations from d

--- P5/P5p5c_init.m
+++ P5/P5p5c_init.m
@@ -1,5 +1,7 @@
-%Acquiring the correct matrices and constants
+%% Define needed constants and initial values for kalman filter and simulation. Load didn't work
+%Constants
 K_pd = 0.816058507166699;
+K = 0.160000000000000;
 psi_ref = 30;
 T = 70.55;
 T_f = 8.395449999999999;
@@ -10,9 +12,24 @@ Q_k = [30 0; 0 10^(-6)];
 I = [1 0 0 0 0; 0 1 0 0 0; 0 0 1 0 0; 0 0 0 1 0; 0 0 0 0 1];
 R = 0.0020*pi^2/180^2;
 ed = [1.752951148958393e-05 0;3.496817831985058e-04 0;0 -3.778486149100418e-07;0 -1.133411979284323e-05;0 0.100000000000000];
+%Initial values
 prior_x_est = [0; 0; 0; 0; 0]; 
 prior_P_covar = [1 0 0 0 0; 0 0.013 0 0 0; 0 0 pi^2 0 0; 0 0 0 1 0; 0 0 0 0 2.5*10^(-3)];
+%Struct for feeding into function
 data = struct('ad', ad, 'bd', bd, 'Cd', Cd, 'ed', ed, 'Q_k', Q_k, 'I', I, 'R',R, 'prior_P_covar',prior_P_covar, 'prior_x_est', prior_x_est);
-plot(compass.time, compass.signals.values, 'b--');
+%% PLOTTING PROBLEM 5d
+figure;
 hold on;
-plot(compass_est.time, compass_est.signals.values, 'r');
+plot(compass_ref.time, compass_ref.signals.values, '--');
+plot(compass.time, compass.signals.values, 'r');
+%plot(compass_est.time, compass_est.signals.values, 'r');
+plot(rudder.time, rudder.signals.values, 'k');
+plot(bias_est.time, bias_est.signals.values, 'g');
+%plot(rudder_comp.time, rudder_comp.signals.values,'-.c');
+legend('Reference for compass angle', 'Measured compass angle', 'Rudder input', 'Estimated bias', 'Rudder input from P5p3c');
+title('Kalman feed forward')
+xlabel('Time[s]');
+ylabel('Degrees[deg]');
+xlim([0 500]);
+ylim auto;
+hold off;
